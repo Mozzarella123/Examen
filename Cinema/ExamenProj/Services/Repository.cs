c49fbbdfa079be6290a33ea06ba5ae35448db4e1
Рогов Никита <Nikita1998_2010@mail.ru>
2018-01-12 13:27:25 +0300

    Репозиторий

---
+++ Cinema/ExamenProj/Services/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExamenProj.Models;
+
+namespace ExamenProj.Services
+{
+    public interface IRepository
+    {
+        DbSet<Film> Films { get; set; }
+        DbSet<Hall> Halls { get; set; }
+        DbSet<Place> Places { get; set; }
+        DbSet<Seans> Seanses { get; set; }
+        DbSet<Ticket> Tickets { get; set; }
+
+    }
+}
